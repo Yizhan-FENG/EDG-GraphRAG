332513f3a7c4822c29bfe8d92ff04782b028bde7
Yizhan FENG <58249200+Yizhan-FENG@users.noreply.github.com>
2026-07-27 15:49:09 +0200

    Create __init__.py

---
+++ src/agent_kg/__init__.py
@@ -0,0 +1,5 @@
+"""电力设备 Agent 知识图谱项目。"""
+
+from .contracts import A1GraphProposal, A2QualityResult, A3DiagnosisDraft, A4ReviewResult
+
+__all__ = ["A1GraphProposal", "A2QualityResult", "A3DiagnosisDraft", "A4ReviewResult"]
